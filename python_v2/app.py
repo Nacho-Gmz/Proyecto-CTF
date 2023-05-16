439ce600e202066f6f7604cb15ddc6e26fdbda94
Nacho-Gmz <luis.gomez5882@alumnos.udg.mx>
2023-05-16 13:05:31 -0600

    Archivos Proyecto

---
+++ python_v2/app.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request, make_response, jsonify
+import os
+import socket
+import random
+import logging
+import psycopg2
+
+
+host = "postgresql.nachogmz-dev.svc.cluster.local"
+conexion = psycopg2.connect(dbname="votingApp", user="Nacho", password="12345", host=host)
+cursor1 = conexion.cursor()
+
+hostname = socket.gethostname()
+option_a = os.getenv('OPTION_A', "Cats")
+option_b = os.getenv('OPTION_B', "Dogs")
+
+
+app = Flask(__name__)
+
+gunicorn_error_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers.extend(gunicorn_error_logger.handlers)
+app.logger.setLevel(logging.INFO)
+
+voter_id = None
+
+@app.route("/", methods=['POST','GET'])
+def hello():   
+    global voter_id 
+    if not voter_id:
+        voter_id = hex(random.getrandbits(64))[2:-1]
+    vote = None    
+   
+
+    if request.method == 'POST':
+        vote = request.form['vote']
+        print(vote)
+        app.logger.info('Received vote for %s', vote)
+        buscar = "SELECT COUNT(*) FROM votos WHERE voter_id = %s;"
+        cursor1.execute(buscar, (voter_id,))
+        
+        if cursor1.fetchone()[0] == 1:
+            actualizar = "UPDATE votos SET vote = %s WHERE voter_id = %s"
+            cursor1.execute(actualizar, (vote, voter_id))
+        else:
+            insertar = "INSERT INTO votos (voter_id, vote) VALUES (%s, %s);"
+            cursor1.execute(insertar, (voter_id, vote))
+        conexion.commit()
+    resp = make_response(render_template(
+        'index.html',
+        option_a=option_a,
+        option_b=option_b,
+        hostname=hostname,
+        vote=vote,
+        voter_id = voter_id
+    ))
+    return resp
+
+@app.route("/cargar_id/<string:id>", methods=['GET','POST'])
+def hola(id = None):
+    if request.method == "POST" and id is not None:
+        global voter_id 
+        voter_id = id
+    return jsonify(code = 'ok')
+    
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8001, debug=True, threaded=True)
+
+conexion.close()
---
+++ result/app.js
@@ -0,0 +1,76 @@
+const { timeEnd } = require('console')
+const express = require('express')
+const app = express()
+const path = require('path')
+const { Client } = require('pg')
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"))
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    const connectionData = {
+        user: 'Nacho',
+        host: 'postgresql.nachogmz-dev.svc.cluster.local',
+        database: 'votingApp',
+        password: '12345',
+        port: 5432,
+      }
+    
+    const client = new Client(connectionData);
+    client.connect();
+    
+    client.query("SELECT vote, COUNT(voter_id) AS count FROM votos GROUP BY vote", 
+        function(err,result){
+            if(err){
+                throw err;
+            }
+            else{
+                var votes = collectVotesFromResult(result);
+                var total = votes.a + votes.b;
+                var cats_percent, dogs_percent, div_izq, div_der;
+        
+                if(total == 0){
+                    cats_percent = 0;
+                    dogs_percent = 0;
+                    div_izq = 50;
+                    div_der = 50;
+                }
+                else{
+                    cats_percent = (votes.a*100)/total;
+                    dogs_percent = (votes.b*100)/total;
+                    div_izq = cats_percent;
+                    div_der = dogs_percent;
+                }
+
+                if(total == 1)
+                    var total_string = total + " voto"
+                else
+                    var total_string = total + " votos"
+                
+                res.render("index", 
+                {
+                    total: total_string,
+                    cats_percent: cats_percent.toFixed(2),
+                    dogs_percent: dogs_percent.toFixed(2),
+                    div_izq: div_izq.toFixed(2),
+                    div_der: div_der.toFixed(2)
+                });
+            }
+        }
+        
+        )
+
+    function collectVotesFromResult(result) {
+        var votes = {a: 0, b: 0};
+          
+        result.rows.forEach(function (row) {
+            votes[row.vote] = parseInt(row.count);
+        });
+          
+        return votes;
+    }
+  
+})
+
+app.listen(3000)
